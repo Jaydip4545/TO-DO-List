2c0fb2d26112e65686041ffb1c1f887052cd3f5b
jaydip mokariya <mokariyajaydip9@gmail.com>
2023-06-10 13:39:48 +0530

    add css

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app=express();
 let w=[];
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
 
 app.get('/',(req,res)=>{
     res.render("index.ejs",{listItems:w});
