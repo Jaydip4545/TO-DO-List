2beacc686a1499c5e26b7aaaef7688451cb61d39
jaydip mokariya <mokariyajaydip9@gmail.com>
2023-06-10 12:26:36 +0530

    Basic to do list

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+let w=[];
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs",{listItems:w});
+})
+
+app.post('/server',(req,res)=>{
+    
+    w.push(req.body.input);
+    res.redirect('/');
+})
+
+
+app.listen(3000);
