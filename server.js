9a016ee2383c1ba156f968f240ca2d61ad8f3ffe
jaydip mokariya <mokariyajaydip9@gmail.com>
2023-06-10 15:25:08 +0530

    add MultiList support

--- server.js
+++ server.js
@@ -2,20 +2,36 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
-let w=[];
+let dailyTasks=[];
+let workTasks=[];
+let title="Daily Tasks";
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 
 app.get('/',(req,res)=>{
-    res.render("index.ejs",{listItems:w});
+    res.render("list.ejs",{listItems:dailyTasks,title:title});
 })
 
-app.post('/server',(req,res)=>{
+app.post('/daily',(req,res)=>{
+
+    if(req.body.button=="Daily Tasks")
+    {
+         dailyTasks.push(req.body.input);
+         res.redirect('/');
+    }
+    else
+    {
+        workTasks.push(req.body.input);
+        res.redirect('/work');
+    }
     
-    w.push(req.body.input);
-    res.redirect('/');
+})
+
+app.get('/work',(req,res)=>{
+    title="Work Tasks";
+    res.render("list.ejs",{listItems:workTasks,title:title});
 })
 
 
